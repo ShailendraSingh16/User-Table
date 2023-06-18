98d52b4f8217f00695a8726d5ea4edf4e1624bae
Shailendra <shailendrasingh16102001@gmail.com>
2023-06-18 14:37:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://dummyjson.com/users')
+      .then((response) => {
+        const data = response.data;
+        if (Array.isArray(data)) {
+          setUsers(data);
+        } else {
+          console.error('Invalid response data:', data);
+        }
+      })
+      .catch((error) => console.error('Error fetching users:', error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>User Table</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Age</th>
+            <th>Gender</th>
+            <th>Email</th>
+            <th>Username</th>
+            <th>Birth Date</th>
+            <th>Image</th>
+            <th>Height</th>
+            <th>Weight</th>
+            <th>Address</th>
+            <th>Bank</th>
+            <th>Company</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.firstname}</td>
+              <td>{user.lastname}</td>
+              <td>{user.age}</td>
+              <td>{user.gender}</td>
+              <td>{user.email}</td>
+              <td>{user.username}</td>
+              <td>{user.birthdate}</td>
+              <td>
+                <img src={user.image} alt="User" height="50" />
+              </td>
+              <td>{user.height}</td>
+              <td>{user.weight}</td>
+              <td>{user.address}</td>
+              <td>{user.bank}</td>
+              <td>{user.company}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
-}
+};
 
 export default App;
